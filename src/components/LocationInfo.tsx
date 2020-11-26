7fe8306af37f6fe15d0990a730e70587a5ae22f2
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-26 14:07:09 +0800

    location info

---
+++ src/components/LocationInfo.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface IProps {
+    data: {
+        title: string | null | undefined;
+        description: string | null | undefined;
+        id: string | null | undefined | number;
+    };
+}
+
+const LocationInfo: React.FC<IProps> = ({ data }) => {
+    return (
+        <div className="info">
+            <h4>ID:{data.id}</h4>
+            <h4>TITLE:{data.title}</h4>
+            <p>DESCRIPTION:{data.description}</p>
+        </div>
+    );
+};
+
+export default LocationInfo;
