840be61bfd450a4da7206fced12e2a590b8e9c5f
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-26 14:04:25 +0800

    app and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Map from "./components/Map";
+
+interface EventsData {
+    id: number | string;
+    title: string;
+    description: string;
+    link: string;
+    categories: Array<any>;
+    sources: Array<any>;
+    geometries: {
+        coordinates: number[];
+        date: string;
+        type: string;
+    }[];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [eventData, setEventData] = useState<EventsData[] | undefined>();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            setIsLoading(true);
+            const res = await fetch(
+                "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+            );
+            const { events } = await res.json();
+            setIsLoading(false);
+            setEventData(events);
+            return events;
+        };
+        fetchEvents();
+    }, []);
+
+    return !isLoading ? (
+        <Map
+            center={{ lat: 42.3265, lng: -122.8756 }}
+            zoom={6}
+            eventData={eventData}
+        />
+    ) : (
+        <div className="loader">LOADING...</div>
+    );
 }
 
 export default App;
